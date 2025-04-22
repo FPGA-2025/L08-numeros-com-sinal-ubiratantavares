b79bc1085af14f084b25af84d25c88f47cf07433
Ubiratan Tavares <ust1973@gmail.com>
2025-04-21 21:53:31 -0300

    implementação do modulo numeros_com_sinal.v

--- numeros_com_sinal.v
+++ numeros_com_sinal.v
@@ -7,6 +7,19 @@ module numeros_com_sinal(
     output reg [7:0] saida
 );
 
-//insira seu codigo aqui
+    always @(*) begin
+        case (codigo)
+            2'b00: // signed_1 + signed_2  
+                saida = entrada_signed_1 + $signed(entrada_signed_2);
+            2'b01: // unsigned_1 + unsigned_2
+                saida = entrada_unsigned_1 + entrada_unsigned_2;
+            2'b10: // unsigned_1 + signed_1
+                saida = entrada_unsigned_1 + entrada_signed_1;
+            2'b11: // unsigned_1 + signed_2
+                saida = entrada_unsigned_1 + $signed(entrada_signed_2);
+            default:
+                saida = 8'b00000000; 
+        endcase        
+    end
 
 endmodule
